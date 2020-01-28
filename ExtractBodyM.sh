32eebfc6bf3a7c6a5c35e787473d721f17c132ae
jessicadeanda <jessicadeanda1@gmail.com>
2020-01-28 10:56:02 -0800

    added ExtractBodyM.sh

--- ExtractBodyM.sh
+++ ExtractBodyM.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-tail -n +2 /home/eeb-c177-student/Developer/repos/CSB/unix/data/Pacifici2013_data.csv | cut -d ";" -f 2-6 | tr -s ";" " " | sort -r -n -k 6 > ExtractBodyM.sh
+tail -n +2 ~/Developer/repos/CSB/unix/data/Pacifici2013_data.csv | cut -d ";" -f 2-6 | tr -s ";" " " | sort -r -n -k 6 > BodyM.csv
