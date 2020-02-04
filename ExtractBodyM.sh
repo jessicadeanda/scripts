75ea4b1954142dead7ca1b7ffe7475cbc11cd7f2
jessicadeanda <jessicadeanda1@gmail.com>
2020-02-04 08:36:08 -0800

    updated assignment

--- ExtractBodyM.sh
+++ ExtractBodyM.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-tail -n +2 ~/Developer/repos/CSB/unix/data/Pacifici2013_data.csv | cut -d ";" -f 2-6 | tr -s ";" " " | sort -r -n -k 6 > BodyM.csv
+INPUTDIR=$1
+tail -n +2 ~/Developer/repos/CSB/unix/data/Pacifici2013_data.csv INPUTDIR | cut -d ";" -f 2-6 | tr -s ";" " " | sort -r -n -k 6 > BodyM.csv
